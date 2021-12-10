2bb0310af03c75d54eb6c67b220088b056a179ac
KarrarAaljaber <karrar_adel99@yahoo.com>
2021-12-09 19:55:57 -0800

    commit

--- src/main/scala/org/example/SparkNeo.scala
+++ src/main/scala/org/example/SparkNeo.scala
@@ -6,6 +6,7 @@ import scala.io.StdIn._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
+
 object SparkNeo extends App {
 
   val spark: SparkSession = SparkSession.builder()
@@ -21,11 +22,11 @@ object SparkNeo extends App {
     .option("labels", "Continent")
     .load()
 
-  val dfCountry = spark.read.format("org.neo4j.spark.DataSource")
+  val dfRelation = spark.read.format("org.neo4j.spark.DataSource")
     .option("url", "bolt://localhost:7687")
     .option("authentication.basic.username", "neo4j")
     .option("authentication.basic.password", "student")
-    .option("labels", "Country")
+    .option("query", "MATCH (source:Country)-[r:LOCATED_IN]->(target:Continent) RETURN source.name, source.GdpPerCapita,target.name")
     .load()
 
   var stopped = false
@@ -34,7 +35,6 @@ object SparkNeo extends App {
 
 
   var continents = dfContinents.select("name").collectAsList()
-  var countries = dfContinents.select("countries").collectAsList()
 
   while(!stopped){
     println("type 1 to add a new country, 2 to view all the countries in the database, 3 to sort by GDP per capita"
@@ -44,71 +44,63 @@ object SparkNeo extends App {
       val country = readLine("Enter the country name: ")
       print("Enter the GDP per Capita for this country:")
       val income = readInt()
+
       println("Which continent is this country located in?(enter a number to choose one)")
       var c =0
       for(c <- 0 to continents.size() - 1){
-          println( c + 1 + " : " + continents.get(c))
+          println( c + " : " + continents.get(c))
       }
+
       val cNum = readInt()
-      if(cNum < 1 || cNum > 5){
+      if(cNum < 0 || cNum > 4){
         println("The number you entered dosent correspond to any of the continents.... quitting program")
         stopped = true
       }
 
 
-      //country node
-      val schema = StructType(Array(
-        StructField("name",StringType,true),
-        StructField("GdpPerCapita",IntegerType,true)
-      ))
-      val temp = Seq(Row(country,income))
+      val data = Seq(
+        (country, income, continents.get(cNum).getString(cNum)))
+      val countryDF = spark.createDataFrame(data).toDF("name", "GdpPerCapita" , "continent")
 
-      val dfForCountry = spark.createDataFrame(
-        spark.sparkContext.parallelize(temp),schema)
-
-      dfForCountry.write.format("org.neo4j.spark.DataSource")
-        .mode(SaveMode.ErrorIfExists)
+      countryDF.write
+        .format("org.neo4j.spark.DataSource")
         .option("url", "bolt://localhost:7687")
         .option("authentication.basic.username", "neo4j")
         .option("authentication.basic.password", "student")
-        .option("labels", "Country")
-        .save()
-      /*
-      val schemaForContinent = StructType(Array(
-        StructField("name",StringType,true),
-        StructField("countries",StringType,true)
-      ))
-
-      val dd = StructType(
-        Array(
-          StructField("name", StringType, true),
-          StructField("countries", ArrayType(StringType), true)
-        )
-      )
-
-      val con = Seq(Row(continents.get(cNum),countries))
-
-      val dfForContinent = spark.createDataFrame(
-        spark.sparkContext.parallelize(con),dd)
-
-      dfForContinent.write.format("org.neo4j.spark.DataSource")
-        .mode(SaveMode.Overwrite)
-        .option("url", "bolt://localhost:7687")
-        .option("authentication.basic.username", "neo4j")
-        .option("authentication.basic.password", "student")
-        .option("labels", "Continent")
+        .option("relationship", "LOCATED_IN")
+        .option("relationship.save.strategy", "keys")
+        .option("relationship.source.labels", ":Country")
+        .option("relationship.source.save.mode", "overwrite")
+        .option("relationship.source.node.keys", "name:name")
+        .option("relationship.source.node.properties", "GdpPerCapita:GdpPerCapita")
+        .option("relationship.target.labels", ":Continent")
+        .option("relationship.target.node.keys", "continent:name")
+        .option("relationship.target.save.mode", "Overwrite")
         .save()
 
-    */
+      countryDF.show()
+
 
 
     }else if(command==2){
-     dfCountry.show(dfCountry.count().toInt,false)
+      dfRelation.show(dfRelation.count().toInt,false)
 
     }else if(command==3){
 
-      println("sorted by GDP per Capita")
-      val gdpTop = dfCountry.sort(col("GdpPerCapita").desc).show()
+      //val gdpTop = dfCountry.sort(col("GdpPerCapita").desc).show()
+
+
+
+
+      println("TOP 10 (sorted by GDP per Capita)")
+      val dd = spark.read.format("org.neo4j.spark.DataSource")
+        .option("url", "bolt://localhost:7687")
+        .option("authentication.basic.username", "neo4j")
+        .option("authentication.basic.password", "student")
+        .option("query", "MATCH (source:Country)-[r:LOCATED_IN]->(target:Continent)  RETURN source.name, source.GdpPerCapita,target.name ORDER BY source.GdpPerCapita DESC")
+        .load()
+
+      dd.show(10,false)
 
     }
 
